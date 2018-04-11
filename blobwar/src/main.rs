786b80a3997cc8fee4a0561f24f58ea6877c2780
Jean Delevoye <jean.delevoye@ensimag.grenoble-inp.fr>
2018-04-11 02:18:40 +0200

    alphabeta done

--- blobwar/src/main.rs
+++ blobwar/src/main.rs
@@ -7,5 +7,5 @@ fn main() {
     //let board = Board::load("x").expect("failed loading board");
     let board = Default::default();
     let mut game = Configuration::new(&board);
-    game.battle(Greedy(), MinMax(4));
+    game.battle(Greedy(), MinMax(6));
 }
--- blobwar/src/strategy/alphabeta.rs
+++ blobwar/src/strategy/alphabeta.rs
@@ -1,6 +1,6 @@
 //! Alpha - Beta algorithm.
 use std::fmt;
-
+use std::cmp;
 use super::Strategy;
 use configuration::{Configuration, Movement};
 use shmem::AtomicMove;
@@ -28,6 +28,70 @@ impl fmt::Display for AlphaBeta {
 
 impl Strategy for AlphaBeta {
     fn compute_next_move(&mut self, state: &Configuration) -> Option<Movement> {
-        unimplemented!("implementer alpha beta")
+        let depth = self.0;
+        let mut mvmt = None;
+        let mut alpha = -100;
+        let mut beta = 100;
+        //maximize
+        if depth % 2 == 1 {
+            println!("Maximize");
+            let mut val = -100;
+            for mov in state.movements() {
+                let cur_val = find_next_move(depth - 1, false, alpha, beta, &Configuration::play(state, &mov));
+                if cur_val > val {
+                    mvmt = Some(mov);
+                    val = cur_val;
+                }
+                if val >= beta {
+                    return mvmt;
+                }
+                alpha = cmp::max(alpha, val);
+            }
+        }
+        //minimize
+        else {
+            println!("Minimize");
+            let mut val = 100;
+            for mov in state.movements() {
+                let cur_val = find_next_move(depth - 1, true, alpha, beta, &Configuration::play(state, &mov));
+                if cur_val < val {
+                    mvmt = Some(mov);
+                    val = cur_val;
+                }
+                if alpha >= val {
+                    return mvmt;
+                }
+                beta = cmp::min(beta, val);
+            }
+        }
+        return mvmt;
+    }
+}
+
+fn find_next_move(depth : u8, maximize : bool, mut alpha : i8, mut beta : i8, state: &Configuration) -> i8 {
+    if depth == 0 {
+        return Configuration::value(state);
+    }
+    if maximize {
+        let mut val = -100;
+        for mov in state.movements() {
+            val = cmp::max(val, find_next_move(depth-1, false, alpha, beta, &Configuration::play(state, &mov)));
+            if val >= beta {
+                return val;
+            }
+            alpha = cmp::max(alpha, val);
+        }
+        return val;
+    }
+    else {
+        let mut val = 100;
+        for mov in state.movements() {
+            val = cmp::min(val, find_next_move(depth-1, true, alpha, beta, &Configuration::play(state, &mov)));
+            if alpha >= val {
+                return val;
+            }
+            beta = cmp::min(beta, val);
+        }
+        return val;
     }
 }
